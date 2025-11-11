2cf74d26e9bff987ad73bb8974f180ccc0953dde
malki23 <52123542+malki23@users.noreply.github.com>
2025-11-11 10:11:40 +0200

    pi

--- CalcPi.java
+++ CalcPi.java
@@ -1,16 +1,19 @@
 public class CalcPi {
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        double sum = 0.0;
+        int n = Integer.parseInt(args[0]);
 
-        for (int i = 0; i < N; i++) {
-            double term = Math.pow(-1, i) / (2.0 * i + 1.0);
-            sum += term;
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sum += 1.0 / (2 * i + 1);
+            } else {
+                sum -= 1.0 / (2 * i + 1);
+            }
         }
 
         double piApprox = 4 * sum;
 
         System.out.println("pi according to Java: " + Math.PI);
-        System.out.println("pi, approximated: " + piApprox);
+        System.out.println("pi, approximated:     " + piApprox); // <-- 5 spaces after colon
     }
 }
