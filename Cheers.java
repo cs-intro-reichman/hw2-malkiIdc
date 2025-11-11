ffa26e9323b4f9fc7eeb15cefaec5c4b56f20c88
malki23 <52123542+malki23@users.noreply.github.com>
2025-11-11 10:08:15 +0200

    cheers

--- Cheers.java
+++ Cheers.java
@@ -1,24 +1,22 @@
 public class Cheers {
         public static void main(String[] args) {
-            String word = args[0];
-            int repeats = Integer.parseInt(args[1]);
-            word = word.toUpperCase();
+            String word = args[0].toUpperCase();
+            int n = Integer.parseInt(args[1]);
     
             String anLetters = "AEFHILMNORSX";
     
-            // Part 1: cheer each letter
+            // For each letter
             for (int i = 0; i < word.length(); i++) {
                 char ch = word.charAt(i);
                 if (anLetters.indexOf(ch) >= 0) {
                     System.out.println("Give me an " + ch + ": " + ch + "!");
                 } else {
-                    System.out.println("Give me a " + ch + ": " + ch + "!");
+                    System.out.println("Give me a  " + ch + ": " + ch + "!"); // double space after 'a'
                 }
             }
     
-            // Part 2: final chant
             System.out.println("What does that spell?");
-            for (int i = 0; i < repeats; i++) {
+            for (int i = 0; i < n; i++) {
                 System.out.println(word + "!!!");
             }
         }
